c7beaa7583290184020f0764332680fd79df727a
Luke Zhao <me@lkzhao.com>
2025-11-10 15:58:08 -0800

    Delete EvalSwiftUI target

--- Sources/EvalSwiftUI/Builder/BuilderProtocols.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-public protocol SwiftUIViewBuilder {
-    var name: String { get }
-    func makeView(arguments: [ResolvedArgument]) throws -> AnyView
-}
--- Tests/EvalSwiftUITests/ExpectSnapshotMacro.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-@freestanding(expression)
-macro expectSnapshot(
-    _ view: Any
-) -> Void = #externalMacro(module: "Macros", type: "SnapshotExpectationMacro")
