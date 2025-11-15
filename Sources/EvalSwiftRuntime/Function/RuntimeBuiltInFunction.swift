95d2d64c4a10888b409f7ac5df42e5efacc28f14
Luke Zhao <me@lkzhao.com>
2025-11-15 09:26:49 -0800

    Delete RuntimeBuiltInFunction.swift

--- Sources/EvalSwiftRuntime/Function/RuntimeBuiltInFunction.swift
+++
@@ -1,6 +0,0 @@
-
-public protocol RuntimeBuiltInFunction {
-    var name: String { get }
-    var parameters: [RuntimeParameter] { get }
-    func call(arguments: [RuntimeArgument], scope: RuntimeScope) throws -> RuntimeValue?
-}
